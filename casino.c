732631ae92d0a3bd5e0078b458b2f942807b7b84
lucas7234 <66056362+lucas7234@users.noreply.github.com>
2020-08-18 12:11:44 +0900

    Update casino.c
    Enter Id(Register)

--- casino.c
+++ casino.c
@@ -21,4 +21,29 @@ main()
   printf("CASINO\n");
   printf("Welcome to Casino Game!\n");
   printf("Register and enjoy a lot of casino games like dice, draw poker!");
+  
+  //variables for register and login
+  char id[9];
+  char password[21];
+  char enterid[9];
+  char enterpw[21];
+  int hasUpper, hasLower, hasDigit, hasSpecial, i;
+  hasUpper=hasLower=hasDigit=hasSpecial=0;
+  
+  //start register
+  do{
+    printf("\nEnter ID(<9 letters)>");
+    scanf(" %s", id);
+  } while (strlen(id)>8);
+  do{
+   printf("\nEnter Password(>21 letters, normal and capital alphabet, number, and special characters required)>") ;
+    scanf(" %s", password);
+    for (i=0; i<strlen(password);i++)
+    {
+      if (isDigit(password[i]))
+      {
+        hasDigit=1;
+        continue;
+    }
+  }
 }
