ac664b485e056d0fdcc0afa60e75ebbf256810b6
lucas7234 <66056362+lucas7234@users.noreply.github.com>
2020-08-18 10:00:03 +0900

    Create casino.c
    Released
    -Greeting

---
+++ casino.c
@@ -0,0 +1,24 @@
+/*Made by lucas119
+casino.c
+Respiratory:Casino*/
+/*you can register and enjoy a lot of casino games*/
+
+//headerfile
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+//constant
+#define TRUE 1
+#define FALSE 0
+
+//main program
+main()
+{
+//Greeting
+  printf("CASINO\n");
+  printf("Welcome to Casino Game!\n");
+  printf("Register and enjoy a lot of casino games like dice, draw poker!");
+}
